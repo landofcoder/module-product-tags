ba35a49ddb95de22ca87add188edfb1dd274acec
HaNguyenQuang <nguyenquangha0209@gmail.com>
2019-08-29 17:27:25 +0700

    Create Database Table and Write setup module file

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -37,6 +37,160 @@ class InstallSchema implements InstallSchemaInterface
         SchemaSetupInterface $setup,
         ModuleContextInterface $context
     ) {
-        //Your install script
+        $installer = $setup;
+		$installer->startSetup();
+		if (!$installer->tableExists('lof_producttags_tags')) {
+			$table = $installer->getConnection()->newTable(
+				$installer->getTable('lof_producttags_tags')
+			)
+                ->addColumn(
+                    'tag_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'Tag ID'
+                )
+                ->addColumn(
+                    'status',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'Status'
+                )
+                ->addColumn(
+                    'tag_title',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Tag Title'
+                )
+                ->addColumn(
+                    'identifier',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Identifier'
+                )
+                ->addColumn(
+                    'created_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_DATETIME,
+                    null,
+                    ['nullable' => false],
+                    'Created At'
+                )
+                ->setComment('News Table')
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+            $installer->getConnection()->addIndex(
+				$installer->getTable('lof_producttags_tags'),
+				$setup->getIdxName(
+					$installer->getTable('lof_producttags_tags'),
+					['tag_title','identifier'],
+					\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+				),
+				['tag_title','identifier'],
+				\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+			);
+        }
+        if (!$installer->tableExists('lof_producttags_product')) {
+			$table = $installer->getConnection()->newTable(
+				$installer->getTable('lof_producttags_product')
+			)
+                ->addColumn(
+                    'tag_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => false,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'foreign' => true
+                    ],
+                    'Tag ID'
+                )
+                ->addColumn(
+                    'product_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'Product ID'
+                )
+                ->addColumn(
+                    'position',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'Position'
+                )
+                ->setComment('News Table')
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+            $installer->getConnection()->addIndex(
+				$installer->getTable('lof_producttags_product'),
+				$setup->getIdxName(
+					$installer->getTable('lof_producttags_product'),
+					[],
+					\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+				),
+				[],
+				\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+			);
+        }
+        if (!$installer->tableExists('lof_producttags_store')) {
+			$table = $installer->getConnection()->newTable(
+				$installer->getTable('lof_producttags_store')
+			)
+                ->addColumn(
+                    'tag_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => false,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'foreign' => true
+                    ],
+                    'Tag ID'
+                )
+                ->addColumn(
+                    'store_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'Store ID'
+                )
+                ->setComment('News Table')
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+            $installer->getConnection()->addIndex(
+				$installer->getTable('lof_producttags_store'),
+				$setup->getIdxName(
+					$installer->getTable('lof_producttags_store'),
+					[],
+					\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+				),
+				[],
+				\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+			);
+		}
+        $installer->endSetup();     
     }
 }
