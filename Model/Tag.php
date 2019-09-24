3f0d899472f9cd192c1cef372cf839b9632c92c4
landofcoder <ecomteck@gmail.com>
2019-09-24 13:25:22 +0700

    updated

--- Model/Tag.php
+++ Model/Tag.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Landofcoder
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * http://landofcoder.com/license
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category   Landofcoder
+ * @package    Lof_ProductTags
+ * @copyright  Copyright (c) 2019 Landofcoder (http://www.landofcoder.com/)
+ * @license    http://www.landofcoder.com/LICENSE-1.0.html
+ */
 namespace Lof\ProductTags\Model;
 use Lof\ProductTags\Api\Data\TagInterface;
 use Lof\ProductTags\Api\Data\TagInterfaceFactory;
